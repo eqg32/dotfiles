bcba22280f434f65582577aabcf92454c0df74d6
ivan <ivan@ivan.ivan>
2024-10-22 18:50:49 +0300

    Updated emacs config

--- dot_emacs.d/extra/use-avy.el
+++ dot_emacs.d/extra/use-avy.el
@@ -1,6 +1,6 @@
-(use-package avy
+(use-package evil-avy
   :ensure t
   :after evil
   :config
-  (define-key evil-normal-state-map "s" 'avy-goto-char-2-below)
-  (define-key evil-normal-state-map "S" 'avy-goto-char-2-above))
+  (define-key evil-normal-state-map (kbd "s") 'evil-avy-goto-char-2-below)
+  (define-key evil-normal-state-map (kbd "S") 'evil-avy-goto-char-2-above))
--- dot_emacs.d/extra/use-org-evil.el
+++
@@ -1,4 +0,0 @@
-(use-package org-evil
-  :ensure t
-  :after evil
-  :hook (org-mode . org-evil-mode))
