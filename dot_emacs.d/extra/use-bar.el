e7f3557658424c839f6afc86ac6cf5cc316ea4bc
ivan <ivan@ivan.ivan>
2024-10-26 22:17:15 +0300

    Updated configs

--- dot_emacs.d/extra/use-bar.el
+++ dot_emacs.d/extra/use-bar.el
@@ -1,4 +1,5 @@
 (use-package mood-line
   :ensure t
   :config
+  (setq mood-line-format mood-line-format-default-extended)
   (mood-line-mode +1))
--- dot_emacs.d/extra/use-doom-themes.el
+++ dot_emacs.d/extra/use-doom-themes.el
@@ -4,6 +4,9 @@
   :config
   (set-frame-parameter nil 'alpha-background 90)
   (load-theme 'doom-nord :no-confirm)
+  (set-face-attribute 'variable-pitch nil
+		      :font "Iosevka Aile"
+		      :height 130)
   (set-face-attribute 'default nil
-		      :font "FiraCode Nerd Font"
-		      :height 120))
+		      :font "Iosevka Nerd Font"
+		      :height 140))
